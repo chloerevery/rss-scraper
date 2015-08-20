081f6bee388df8ac9c89ceff4173e084e66babab
acirstea <aicirstea@gmail.com>
2015-08-19 18:53:13 -0700

    Update script.rb

--- script.rb
+++ script.rb
@@ -1,31 +1,13 @@
 require 'feedjira'
 
+
 url = "http://www.hackthehood.org/4/feed"
 feed = Feedjira::Feed.fetch_and_parse url
 
 
-# feed.entries.each do |entry|
-# 	# when we move to rails:
-# 	# feed_entry = FeedEntry.new
-# 	puts "title: " + entry.title
-
-# 	puts "url: " + entry.url
-
-# 	puts "last modified: " + entry.last_modified.inspect
-# end
-
-#[:@title, :@url, :@published, :@categories, :@entry_id, :@summary, :@content]
-
-# $i = 0
-# $num = 5
-# begin
-#    puts("Inside the loop i = #$i" )
-#    $i +=1
-# end while $i < $num
-
 $i = 0
 $num = feed.entries.length
-begin
+while $i < $num
 	puts("i is: " + $i.inspect)
 	puts("")
 	puts("title" + feed.entries[$i].title)
@@ -34,20 +16,41 @@ begin
 	puts("")
 	puts("published: " + feed.entries[$i].published.inspect)
 	puts("")
+	
 	feed.entries[$i].categories.each do |category|
 		puts category
 	end
+
 	#puts("categories: " + feed.entries[$i].categories)
 	puts("")
 	puts("entry id: " + feed.entries[$i].entry_id)
 	puts("")
-	puts("summary: " + feed.entries[$i].summary)
+	puts("descirption: " + feed.entries[$i].summary)
 	puts("")
 	puts("content: " + feed.entries[$i].content)
+	puts("")
+	### parse deadline ###
+	$contentString = (feed.entries[$i].content).downcase
+	puts("contentString " + $contentString)
+	$contentStringIndex = $contentString.index("deadline")
+	puts("contentStringIndex " + $contentStringIndex.inspect)
+	$str = ""
+	$currentCharIndex = $contentStringIndex
+
+	if $currentCharIndex != nil
+		while $contentString[$currentCharIndex] != '<'
+			$str = $str+$contentString[$currentCharIndex]
+			$currentCharIndex+=1
+		end 
+	else
+		$str = "Rolling deadline."
+	end
+	puts("String is: " + $str)
+	### done parsing deadline ### 
 	$i += 1
 	puts("")
 	puts("")
 	puts("")
 	puts("")
 	puts("")
-end while $i<$num
+end
